2f71818a3ef10059d5503a30076eb719c0dc44d9
Elhakim <elhakimdev@gmail.com>
2019-09-24 14:40:54 +0200

    Finish adding list items

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import {getLineAtPosition, isListItem} from '../util/newListItem';
+
+let clickedEnter = false;
 
 export interface MdeEditorProps {
   value: string;
@@ -16,13 +19,89 @@ export interface MdeEditorProps {
   >;
 }
 
-export class TextArea extends React.Component<MdeEditorProps, {}> {
-  handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { onChange } = this.props;
-    onChange(event.target.value);
+export interface Insertion {
+  text: string;
+  position: number;
+}
+
+export const insertText = (text: string, position: number, insertText: string): Insertion => {
+  const start = text.slice(0, position);
+  const end = text.slice(position);
+  return {
+    text: start + insertText + end,
+    position: (start + insertText).length
   };
+}
+
+function setCaretPosition(caretPos) {
+  const elem:any = document.getElementById('mde-textarea');
+  elem.value = elem.value
+  if(elem != null) {
+      if(elem.createTextRange) {
+          var range = elem.createTextRange();
+          range.move('character', caretPos);
+          range.select();
+          return true;
+      }
+      else {
+          if(elem.selectionStart || elem.selectionStart === 0) {
+              elem.focus();
+              elem.setSelectionRange(caretPos, caretPos);
+              return true;
+          }
+          else
+              elem.focus();
+              return false;
+      }
+  }
+}
+
+export class TextArea extends React.Component<MdeEditorProps, {}> {
+
+  handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (!clickedEnter) {
+      const { onChange } = this.props;
+      let { value } = event.target;
+      onChange(value);
+    }
+    clickedEnter = false
+  };
+
   handleOnBlur = () => {
     this.props.onBlur(this.props.value)
+  }
+
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      clickedEnter = true;
+      let {selectionStart, value} = event.target;
+      let currentLine = getLineAtPosition(value, selectionStart)
+      let newValue: string = "\n";
+
+      // Add spaces of the line.
+      if (isListItem(currentLine.type)) {
+        for (let i = 0; i < currentLine.treePosition * 4; i++) {
+          newValue += ' ';
+        }
+      }
+
+      // Add prefix of the line
+      if (currentLine.type === 'ol') {
+        newValue += currentLine.order + '. '
+      } else {
+        newValue += currentLine.prefix
+      }
+
+      const insertion: Insertion = insertText(event.target.value, event.target.selectionStart, newValue)
+      this.props.onChange(insertion.text);
+      
+      setTimeout(() => {
+        setCaretPosition(insertion.position);
+        clickedEnter = false;
+      }, 0)
+
+    }
   }
 
   render() {
@@ -37,12 +116,14 @@ export class TextArea extends React.Component<MdeEditorProps, {}> {
     return (
       <textarea
         dir="rtl"
+        id="mde-textarea"
         className={`mde-text ${className || ""}`}
         style={{ height }}
         ref={editorRef}
         onChange={this.handleOnChange}
         readOnly={readOnly}
         onBlur={this.handleOnBlur}
+        onKeyPress={this.handleKeyPress}
         required
         value={value}
         {...textAreaProps}
---
+++ src/util/newListItem.ts
@@ -0,0 +1,128 @@
+export interface Line {
+    /** Line index of all lines */
+    index: number;
+    /** the pure text of the line */
+    text: string;
+    /** Line type `ordered list` | `unordered list` | `check list` | 'empty line` | `other` */
+    type: 'ul' | 'ol' | 'cl' | 'empty' | 'other';
+    /** item prefix if is a list item */
+    prefix?: string;
+    /** the order of the line in the list if is a list item */
+    order?: number;
+    /** Either the list item is checked or not if the type is a `check list` */
+    checked?: boolean;
+    /**
+     * only if is a list item line. the position in the ul tree
+     * => 0 = is a parent, 1 = the first child list, 2 = the second child list
+     * and so on.
+     */
+    treePosition?: number;
+  }
+  
+  interface ListItem {
+    /** Line type `ordered list` | `unordered list` | `check list` | 'empty line` | `other` */
+    type: 'ul' | 'ol' | 'cl' | 'empty' | 'other';
+    /** item prefix if is a list item */
+    prefix: string;
+    /** the order of the line in the list if is a list item */
+    order?: number;
+    /** Either the list item is checked or not if the type is a `check list` */
+    checked?: boolean;
+    /**
+     * only if is a list item line. the position in the ul tree
+     * => 0 = is a parent, 1 = the first child list, 2 = the second child list
+     * and so on.
+     */
+    treePosition: number;
+  }
+  
+  export const isListItem = (type:string):boolean => ['ol', 'ul', 'cl'].includes(type)
+  
+  const getItem = (line: string):ListItem => {
+    let _line = line.trimLeft();
+    let spaces: string;
+    let treePosition: number;
+  
+    // If unchecked item in check list
+    if (!!_line.match(/^- \[ ] /g)) {
+      spaces = line.split('- [ ] ')[0];
+      treePosition = spaces.length % 4 === 0? spaces.length / 4: 0
+      return {
+        type: 'cl',
+        prefix: '- [ ] ',
+        checked: false,
+        treePosition
+      }
+    };
+  
+    // if checked item in check list
+    if (!!_line.match(/^- \[x] /g)) {
+      spaces = line.split('- [x] ')[0];
+      treePosition = spaces.length % 4 === 0? spaces.length / 4: 0
+      return {
+        type: 'cl',
+        prefix: '- [x] ',
+        checked: true,
+        treePosition
+      }
+    };
+  
+    // if unordered list item
+    if (!!_line.match(/^- /g)) {
+      spaces = line.split('- ')[0];
+      treePosition = spaces.length % 4 === 0? spaces.length / 4: 0
+      return {
+        type: 'ul',
+        prefix: '- ',
+        treePosition
+      }
+    };
+  
+    // if ordered list item
+    if (!!_line.match(/^[0-9]*. /g)) {
+      let prefix = _line.match(/^[0-9]*. /g)[0];
+      let order = Number(prefix.split('.')[0]) + 1
+      spaces = line.split(prefix)[0]
+      treePosition = spaces.length % 4 === 0? spaces.length / 4: 0
+      return {
+        type: 'ol',
+        prefix,
+        treePosition,
+        order
+      }
+    };
+  
+    // if an empty line
+    if (_line.length === 0) {
+      return {
+        type: 'empty',
+        prefix: '',
+        treePosition: 0
+      }
+    };
+  
+    // if another line
+    return {
+      type: 'other',
+      prefix: '',
+      treePosition: 0
+    }
+  }
+  
+  /** Return the line that the cursor exists in */
+  export const getLineAtPosition = (text: string, position: number) :Line => {
+    let lines: string[] = text.split('\n');
+    let line: Line;
+    lines.forEach((l:string, index:number) => {
+      position -= (l.length + 1);
+      let item: ListItem = getItem(l);
+      if (!line && position <= 0) {
+        line = {
+          ...item,
+          index,
+          text: l
+        };
+      }
+    })
+    return line;
+  }
